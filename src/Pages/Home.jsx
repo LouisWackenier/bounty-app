1b7f79aea5a21a7710c928fc8e458fd176e0c23a
LouisWackenier <Louis.wackenier@gmail.com>
2023-12-01 06:55:56 +0100

    Main title

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,12 +6,15 @@ const MainContainer = styled.div`
 
 const MainTitle = styled.div`
      font-weight: 800;
+     font-size: 3rem;
+     text-align: center;
+     padding-top: 10vh;
 `;
 
 const Home = () => {
   return (
     <MainContainer>
-           <MainTitle>This is going to be the home</MainTitle>
+           <MainTitle>This is going to be the home page</MainTitle>
     </MainContainer>
   )
 }
