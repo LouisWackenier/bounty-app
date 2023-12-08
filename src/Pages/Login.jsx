8abb51dff82517d0b9aee5b33f62250351e07cc5
LouisWackenier <Louis.wackenier@gmail.com>
2023-12-08 09:05:48 +0100

    Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,34 @@
 import styled from "styled-components";
 
 const MainContainer  = styled.div`
-         height: 80vh;
+         height: 50vh;
+`;
+
+const FormContainer = styled.div`
+    margin-left: 45vw;
+    margin-top: 30vh;
+`;
+
+const FormPassword = styled.form`
+
+`;
+
+const FormUsername = styled.form`
+
+`;
+
+const FormEmail = styled.form`
+
 `;
 
 const Login = () => {
   return (
     <MainContainer>
-        Here is gonna be the Login section
+        <FormContainer>
+             <FormEmail>Email</FormEmail>
+             <FormUsername>Username</FormUsername>
+             <FormPassword>Password</FormPassword>
+        </FormContainer>
     </MainContainer>
   )
 }
