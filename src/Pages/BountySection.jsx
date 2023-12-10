0ac2a99742a86b49058f311ed66d557ab57e3e06
LouisWackenier <Louis.wackenier@gmail.com>
2023-12-10 09:05:48 +0100

    made the bounty card

--- src/Pages/BountySection.jsx
+++ src/Pages/BountySection.jsx
@@ -7,12 +7,35 @@ const MainContainer = styled.div`
 const BountyContainer = styled.div`
       width: 250px;
       height: 300px;
+
+      margin-left: 8vw;
+      margin-top: 2vh;
+      text-align: center;
+`;
+
+const BountyPrice = styled.div`
+
       background-color: gray;
+      color: black;
+      font-weight: 700;
+`;
 
-      margin-left: 5vw;
-      margin-top: 5vh;
+const BountyPicture = styled.div`
+      width: 250px;
+      height: 150px; 
+      padding-top: 100px; 
 
+      background-color: gray;
+      color: black;
+      font-weight: 700;
+`;
 
+const BountyPriceSelect = styled.div`
+      height: 40px;
+
+      background-color: gray;
+      color: black;
+      font-weight: 700;
 `;
 
 const BountySection = () => {
@@ -20,7 +43,9 @@ const BountySection = () => {
     <MainContainer>
          {/*Bounty comes here*/}   
          <BountyContainer>
-
+               <BountyPicture>Image goes here</BountyPicture>
+               <BountyPrice>100</BountyPrice>
+               <BountyPriceSelect>Select the price</BountyPriceSelect>
          </BountyContainer>
     </MainContainer>
   )
