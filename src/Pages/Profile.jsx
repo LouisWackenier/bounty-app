c35c85a6477869ee456ad33589d834f60504e645
LouisWackenier <Louis.wackenier@gmail.com>
2023-12-17 09:19:07 +0100

    Profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+      height: 80vh;
+`;
+
+const profilePicture = styled.div`
+      width: 150px;
+      height: 150px;
+      background-color: gray;
+      border-radius: 50%;
+`;
+
+const profileName = styled.div`
+      
+`;
 
 const Profile = () => {
   return (
-    <div>This page displays your profile</div>
+    <MainContainer>
+           <profilePicture>pfp</profilePicture>
+           <profileName>John Doe</profileName>
+    </MainContainer>
   )
 }
 
